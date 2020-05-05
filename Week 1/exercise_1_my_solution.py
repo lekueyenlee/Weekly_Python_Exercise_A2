078e203fbe1888d10d33201f44f97679f6ceaca1
lekueyenlee <lekueyen.lee@gmail.com>
2020-05-05 16:18:17 -0500

    Update exercise_1_my_solution.py
    Created a method container, method takes in a string, returns the string, and outputs out the value.

---
+++ Week 1/exercise_1_my_solution.py
@@ -0,0 +1,11 @@
+# Exercise 1 
+# Description: Create an acroym from the string
+# Date: 05/05/2020
+
+
+def acrobat(input_string_value):
+    string_val = input_string_value
+
+    return string_val
+
+print(acrobat("Here is my String"))
